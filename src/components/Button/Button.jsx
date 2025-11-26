162029eb08e44dc483798b5afbee37eddc4c970c
EZH <stellage.ua@gmail.com>
2025-11-26 19:07:54 +0200

    3

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,12 @@
 import {} from "react";
 import cls from "./Button.module.css";
 const isPrimary = true;
-export const Button = () => {
-  return <button className={isPrimary ? cls.primary : cls.btn}>Click</button>;
+export const Button = (props) => {
+  console.log(props);
+  return (
+    <button className={`${cls.btn} ${isPrimary ? cls.primary : ""}`}>
+      Click
+    </button>
+  );
+  // <button className={isPrimary ? cls.primary : cls.btn}>Click</button>;
 };
